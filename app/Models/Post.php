5758b9306c260e7aa3249ffc7c91b74c18ba6ac6
YaqoubYousef <yaqoubyousef84@gmail.com>
2025-09-24 14:55:10 +0300

    Initial commit

---
+++ app/Models/Post.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Category;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'title',
+        'content',
+        'excerpt',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+    //
+}
+
+
+
